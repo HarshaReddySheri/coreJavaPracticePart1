172b4f105381ca620307d80cc556bfa9644e0e73
HarshaReddySheri <sherigopalreddy1975@gmail.com>
2023-10-08 11:42:28 +0530

    fourth commit coreJavaPracticePart1

---
+++ coreJavaPracticePart1/src/AbstractionAndEncapuslationHidingIsCompletelyDifferent.java
@@ -0,0 +1,12 @@
+
+public class AbstractionAndEncapuslationHidingIsCompletelyDifferent {
+	/*
+	 * Encapsulation -> Hiding the data (not giving direct data) through the methods
+	 * *************
+	 * Only data hiding & we can't see any implementation in the method
+	 * 
+	 * Abstraction -> Hiding the implementation details showing only functionality to the user
+	 * ***********
+	 * Entire implementation you are hiding 
+	 */
+}
